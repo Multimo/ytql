26b9c9754df35a12728db6a79dd8766535c95eaf
Tim <tmulqueen@premiumbeat.com>
2017-05-07 13:53:36 -0400

    init

---
+++ fakeDb.js
@@ -0,0 +1,99 @@
+export const fakeDB = {};
+const users = {
+    "users": [{
+            "KeffwUIrItL": {
+                "id": "Kef04N5BuhCPwUIrItL",
+                "email": "fake@123.com",
+                "name": "faker",
+                "savedPlaylists": [
+                    "Kef04N5BuhCPwUIrItL",
+                    "Kef04N5BuhCPwUIrItL"
+                ]
+            }
+        },
+        {
+            "dsaafdsfd": {
+                "id": "Kef04N5BuhCPwUIrItL",
+                "email": "fake@123.com",
+                "name": "faker",
+                "savedPlaylists": [
+                    "Kef04N5BuhCPwUIrItL",
+                    "Kef04N5BuhCPwUIrItL"
+                ]
+            }
+        },
+        {
+            "Kef04N5BuhCPwUIrItL": {
+                "id": "Kef04N5BuhCPwUIrItL",
+                "email": "Basake@123.com",
+                "name": "Bsaaker",
+                "savedPlaylists": [
+                    "Kef04N5BuhCPwUIrItL",
+                ]
+            }
+        }
+    ]
+}
+
+const Rooms = {
+    "rooms": [{
+            "id": "-KeerJ1lt7q6QoJEZxRf",
+            "roomDesc": "songs for Dog",
+            "roomName": "Dog",
+            "roomPlaylist": "Kef04N5BuhCPwUIrItL",
+            'users': [
+                "KeffwUIrItL",
+                "dsaafdsfd",
+                "Kef04N5BfdjsaklfjdItfL"
+            ]
+        },
+        {
+            "id": "-KeerJ1lt7q6QoJEZxRf",
+            "roomDesc": "Best Rooms here",
+            "roomName": "Good ROom",
+            "roomPlaylist": "Kef04N5BuhCPwUIrItL",
+            'users': [
+                "KeffwUIrItL",
+                "dsaafdsfd",
+                "Kef04N5BfdjsaklfjdItfL"
+            ]
+        },
+        {
+            "id": "-KeerJ1lt7q6QoJEZxRf",
+            "roomDesc": "Bad rooms for Dog",
+            "roomName": "Bad ROoms",
+            "roomPlaylist": "Kef04N5BuhCPwUIrItL",
+            'users': [
+                "KeffwUIrItL",
+                "dsaafdsfd",
+                "Kef04N5BfdjsaklfjdItfL"
+            ]
+        }
+    ]
+}
+
+// Playlists
+const Playlists = {
+    "playlists": [{
+        "Kef04N5BuhCPwUIrItL": {
+            "playlistId": "Kef04N5BuhCPwUIrItL",
+            "playlistDesc": "Trogdorrr",
+            "playlist": [{
+                "title": "newerSongsForDog",
+                "url": "www.youtube.com/newerSongsForDog",
+                "thumbnail": "songthumburl",
+                "songName": "Falcor the urinator"
+            }, {
+                "title": "twoTangosPlease",
+                "url": "www.youtube.com/TAMid",
+                "thumbnail": "songthumburl",
+                "songName": "Mid or feed"
+            }, {
+                "title": "needWards",
+                "url": "www.youtube.com/30gtoblink",
+                "thumbnail": "songthumburl",
+                "songName": "talesoflion"
+            }]
+        }
+    }]
+}
